3d4fb7b1f40329ef60c1592c7c6d20dd6d014c60
Corey Auger <coreyauger@gmail.com>
2018-07-10 16:02:55 -0700

    initial

--- engine/graphics/vulkan/VulkanGraphics.hpp
+++ engine/graphics/vulkan/VulkanGraphics.hpp
@@ -1,22 +1,64 @@
 #ifndef TRB_GFX_VulkanGraphics_H_
 #define TRB_GFX_VulkanGraphics_H_
 
+#define GLFW_INCLUDE_VULKAN
+#include <vulkan/vulkan.hpp>
+#include <GLFW/glfw3.h>
+
+#include <set>
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+#include <cstring>
+#include <cstdlib>
+#include <algorithm>
+#include <fstream>
 #include "../GraphicsInterface.hpp"
+
+
+//////
+// Validation Layer Callbacks
+
+VkResult CreateDebugReportCallbackEXT(VkInstance instance, const VkDebugReportCallbackCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugReportCallbackEXT* pCallback) {
+    auto func = (PFN_vkCreateDebugReportCallbackEXT) vkGetInstanceProcAddr(instance, "vkCreateDebugReportCallbackEXT");
+    if (func != nullptr) {
+        return func(instance, pCreateInfo, pAllocator, pCallback);
+    } else {
+        return VK_ERROR_EXTENSION_NOT_PRESENT;
+    }
+}
+
+void DestroyDebugReportCallbackEXT(VkInstance instance, VkDebugReportCallbackEXT callback, const VkAllocationCallbacks* pAllocator) {
+    auto func = (PFN_vkDestroyDebugReportCallbackEXT) vkGetInstanceProcAddr(instance, "vkDestroyDebugReportCallbackEXT");
+    if (func != nullptr) {
+        func(instance, callback, pAllocator);
+    }
+}
+
+// END: Validation Layer Callbacks
+/////
 
 namespace trb{
     namespace grfx{
         
-        class VulkanGraphics : public GraphicInterface{
-            private:
-                VulkanGraphics(){}
-            public:                
+        class VulkanGraphics : public GraphicInterface{            
+            public:                                
+                void init(){
+                    initWindow();
+                    initVulkan();
+                }
 
-                static VulkanGraphics* create(){
+                static VulkanGraphics* create(bool enableValidationLayers){
                     VulkanGraphics *graphics = new VulkanGraphics();
-                    // do the init.
+                    graphics->init();
                     return graphics;
                 }
 
+            private:
+                VulkanGraphics(){}
+
+                void initWindow();
+                void initVulkan();
         };
     }
 }
